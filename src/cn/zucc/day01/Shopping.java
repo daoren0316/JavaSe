0e30d3474b5721179568eea0e87231bd58d13272
daoren <477954742@qq.com>
2015-10-19 14:16:10 +0800

    1.初始化项目

---
+++ src/cn/zucc/day01/Shopping.java
@@ -0,0 +1,31 @@
+package cn.zucc.day01;
+
+public class Shopping {
+
+	/**
+	 * @param args
+	 *转义符
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\t\t购物清单");
+		System.out.print("商品名称");
+		System.out.print("\t\t购买数量");
+		System.out.print("\t\t商品单价");
+		System.out.print("\t\t金额\n");
+		
+		System.out.print("鳄鱼夹克");
+		System.out.print("\t\t1");
+		System.out.print("\t\t1900");
+		System.out.print("\t\t1900\n");
+		
+		System.out.print("ipodmp4");
+		System.out.print("\t\t2");
+		System.out.print("\t\t400");
+		System.out.print("\t\t800\n");
+		
+		System.out.println("******************************");
+		
+	}
+
+}
